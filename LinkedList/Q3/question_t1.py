544f48450389d359a2dbd1797defda9a1ef2835c
thewashedcastle <55625081+thewashedcastle@users.noreply.github.com>
2020-12-01 23:31:43 +0530

    question 3 applied

---
+++ LinkedList/Q3/question_t1.py
@@ -0,0 +1,28 @@
+import generateLinkedList as ll
+import random
+
+head,length = ll.createLinkedList()
+ll.printlinked(head)
+
+
+def randomNode(head):
+    print(length,"length")
+    randomNodeIdx = random.randrange(length)
+    print(randomNodeIdx,"randomIdx")
+    for i in range(randomNodeIdx):
+        head = head.next
+    return head
+
+randomIdxNode = randomNode(head)
+
+def deleteRandomNode(pointer1 , pointer2):
+    while pointer2.next != None:
+        pointer1.value = pointer2.value
+        pointer1 = pointer1.next
+        pointer2 = pointer2.next
+    
+    pointer1.next = None
+
+
+deleteRandomNode(randomIdxNode , randomIdxNode.next)
+ll.printlinked(head)
