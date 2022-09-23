e6f495d34b25a3d226c63dee907df05502718938
Calvin Thomas Dani <calvinthomas.dani@gmail.com>
2022-09-22 21:23:25 -0700

    Added git ignore and fixed two pointer runner

--- LinkedList/Q8/question_t2.py
+++ LinkedList/Q8/question_t2.py
@@ -5,11 +5,33 @@ def checkIfLooped(linkedL):
     pointerOne = linkedL
     pointerTwo = linkedL.next
 
-    while not pointerOne is pointerTwo:
+    while not (pointerOne is pointerTwo) and pointerTwo is not None:
         pointerOne = pointerOne.next
         pointerTwo = pointerTwo.next.next
 
-    return pointerTwo.next
+    lengthOfCycle = 0
+    if pointerOne is pointerTwo:
+        pointerOne = pointerOne.next
+        lengthOfCycle = 1
+        while pointerOne is not pointerTwo:
+            pointerOne = pointerOne.next
+            lengthOfCycle += 1
+
+    return (True,lengthOfCycle) if pointerTwo is pointerOne else (False,lengthOfCycle)
+
+def checkCycleStart(linkedL,lengthOfCycle):
+    pointerOne = linkedL
+    pointerTwo = linkedL
+    for step in range(lengthOfCycle):
+        pointerTwo = pointerTwo.next
+    
+    while pointerOne is not pointerTwo:
+        pointerOne = pointerOne.next
+        pointerTwo = pointerTwo.next
+
+    return pointerOne
+        
+
 
 
 def main():
@@ -17,8 +39,10 @@ def main():
     integerOne, lengthIntegerOne = ll.createLoopLL()
     ll.printlinked(integerOne)
 
-    val = checkIfLooped(integerOne)
-    print("integerOne loop begins at", val, val.value)
-
+    checkLoop,lengthOfCycle = checkIfLooped(integerOne)
+    if checkLoop:
+        val = checkCycleStart(integerOne,lengthOfCycle)
+        print("integerOne loop begins at", val, val.value)
+    print("This linked list is acyclic")
 
 main()
